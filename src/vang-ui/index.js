215a0789450088689499f743b759a2b0acc22518
Sergei Kundryukov <sergei.kundryukov1989@gmail.com>
2020-12-01 11:29:37 +0300

    Create vang-ui app

--- src/vang-ui/index.js
+++ src/vang-ui/index.js
@@ -1,2 +1,30 @@
 #!/usr/bin/env node
-console.log('hello world!');
+const readline = require('readline');
+const input = readline.createInterface(process.stdin);
+const multiplyer = parseInt(Math.random() * 100);
+const hiddenNumber = parseInt(Math.random() * multiplyer);
+
+process.stdout.write(`Загадано число в диапозоне от 0 до ${multiplyer}\n`);
+
+input.on('line', msg => {
+    const inputedNumber = Number(msg);
+    if (!msg || !isFinite(inputedNumber)) {
+        process.stdout.write(`Пожалуйста, введите корректное целое число. Предыдущее значение: "${msg}"\n`);
+        return;
+    }
+
+    if (inputedNumber === hiddenNumber) {
+        process.stdout.write(`Отгадано число ${msg}!`);
+        process.exit(0);
+    }
+
+    if (inputedNumber > hiddenNumber) {
+        process.stdout.write('Меньше\n');
+        return;
+    }
+
+    if (inputedNumber < hiddenNumber) {
+        process.stdout.write('Больше\n');
+        return;
+    }
+});
